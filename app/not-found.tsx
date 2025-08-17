14aa7cf0c518a7a40dabcd7387efd18ca0e54498
Akshay Kale <162922094+Dev-axay18@users.noreply.github.com>
2025-08-17 10:14:24 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,17 +4,24 @@ import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div className="flex h-[100vh] flex-col items-center justify-center bg-[#0E1016] text-[#e4ded7] gap-5">
-      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">Annnaa… this ain’t the rooooad (Error 404)</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#0E1016] text-[#e4ded7] gap-5 p-6">
+      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] text-center uppercase">
+        Annnaa… this ain’t the rooooad (Error 404)
+      </p>
+      
       <Image
         src="https://raw.githubusercontent.com/Dev-axay18/axayportfolio/main/public/stressed-screamibg.png"
         alt="Funny Meme"
         width={858}
         height={483}
-        className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%]"
+        className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] max-h-[60vh] object-contain"
       />
+
       <div>
-        <Link href="/" className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">
+        <Link 
+          href="/" 
+          className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase"
+        >
           Head back to my{" "}
           <span className="underline underline-offset-2">Portfolio</span>
         </Link>
